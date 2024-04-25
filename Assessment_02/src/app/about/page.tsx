e06a0529839fa53158a9ea76711f876a47fffb73
Dimuth-J <dimuthdimuth9@gmail.com>
2024-04-25 23:36:33 +0530

    ss

--- Assessment_02/src/app/about/page.tsx
+++ Assessment_02/src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div>
         <h1 className="text-3xl font-bold underline">
-          About
+          Abouttsdhagsdjashg
         </h1>
     </div>
   )
