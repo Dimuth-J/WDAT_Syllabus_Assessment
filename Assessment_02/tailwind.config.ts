a65698c76b274d891b181067428c61f0ab6929d8
Dimuth-J <dimuthdimuth9@gmail.com>
2024-04-25 19:53:24 +0530

    check tailwind

--- Assessment_02/tailwind.config.ts
+++ Assessment_02/tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "#fff",
+        darkslateblue: "#24386c",
+        lightseagreen: "#00967b",
+      },
+      spacing: {},
+      fontFamily: {
+        inter: "Inter",
+      },
+      borderRadius: {
+        "31xl": "50px",
+      },
+    },
+    fontSize: {
+      "17xl": "36px",
+      "3xl": "22px",
+      "10xl": "29px",
+      inherit: "inherit",
+    },
+    screens: {
+      mq767: {
+        raw: "screen and (max-width: 767px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
     },
   },
   plugins: [],
